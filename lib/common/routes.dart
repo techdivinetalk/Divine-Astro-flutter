ac1372c04ba1da3a6f80dc9ce04f049cacc2253d
chauhanritaIOS <rita@plannet.io>
2023-08-01 19:29:45 +0530

    -Working on profile screen UI.(In progress)

--- lib/common/routes.dart
+++ lib/common/routes.dart
@@ -1,12 +1,15 @@
+import 'package:divine_astrologer/screens/profile/profile_binding.dart';
+import 'package:divine_astrologer/screens/profile/profile_ui.dart';
 import 'package:get/get.dart';
 
 import '../screens/splash/splash_binding.dart';
 import '../screens/splash/splash_ui.dart';
 
 class RouteName {
-  static const initial = root;
+  static const initial = profilePage;
 
   static const String root = "/";
+  static const String profilePage = "/profilePage";
 }
 
 class Routes {
@@ -15,5 +18,9 @@ class Routes {
         page: () => const SplashUI(),
         name: RouteName.root,
         binding: SplashBinding()),
+    GetPage(
+        page: () => const ProfileUI(),
+        name: RouteName.profilePage,
+        binding: ProfileBinding()),
   ];
 }
