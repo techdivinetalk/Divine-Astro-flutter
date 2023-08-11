55d91b4a1fe94498e48f962a98ce0601938a57ff
Khyati Modi <khyati.modi@expertappdevs.dev>
2023-08-11 19:48:03 +0530

    Update firebase network service.

--- lib/di/firebase_network_service.dart
+++ lib/di/firebase_network_service.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: deprecated_member_use
 
 import 'dart:async';
-import 'package:firebase_core/firebase_core.dart';
+
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -13,23 +13,31 @@ class FirebaseNetworkService extends GetxService {
   Stream<bool?> get databaseConnectionStream => _onDatabaseConnected.stream;
 
   Future<FirebaseNetworkService> init() async {
-    DatabaseReference firebaseDatabase = FirebaseDatabase.instanceFor(
-            app: Firebase.app(),
-            databaseURL: "https://divine2-0-uat-default-rtdb.firebaseio.com/")
-        .ref(".info/connected");
-
-    // DatabaseReference firebaseDatabase = FirebaseDatabase(
+    // DatabaseReference firebaseDatabase = FirebaseDatabase.instanceFor(
+    //         app: Firebase.app(),
     //         databaseURL: "https://divine2-0-uat-default-rtdb.firebaseio.com/")
     //     .ref(".info/connected");
 
-    firebaseDatabase.onValue.listen((event) {
+    FirebaseDatabase firebaseDatabase = FirebaseDatabase(
+        databaseURL: "https://divine2-0-uat-default-rtdb.firebaseio.com/");
+
+    Stream<DatabaseEvent> infoConnect =
+        firebaseDatabase.ref().child(".info/connected").onValue;
+
+    Stream<DatabaseEvent> randomCheck =
+        firebaseDatabase.ref().child("astrologer/200/watcher").onValue;
+
+    infoConnect.listen((event) {
       final connected = event.snapshot.value as bool? ?? false;
       _onDatabaseConnected.sink.add(connected);
       if (connected) {
-        debugPrint("You are Connected");
+        debugPrint("You are Connected.");
       } else {
-        debugPrint("You are Not Connected");
+        debugPrint("You DisConnected");
       }
+    });
+    randomCheck.listen((event) {
+      debugPrint("Value has been updated: ${event.snapshot.value}");
     });
 
     return this;
