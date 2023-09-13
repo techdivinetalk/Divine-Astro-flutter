d5b42a270ade3a1c9eefdd189b81b348084d89b2
Divit V <divit.vaghani@expertappdevs.dev>
2023-09-13 19:50:03 +0530

    -added dynamic icons in waiting list ui.

--- lib/screens/side_menu/wait_list/wait_list_ui.dart
+++ lib/screens/side_menu/wait_list/wait_list_ui.dart
@@ -54,8 +54,10 @@ class WaitListUI extends GetView<WaitListUIController> {
                   SizedBox(
                     height: 15.h,
                   ),
-                  waitingListTile(controller.waitingPersons[0].getCustomers!,
-                      controller.waitingPersons[0].waitTime ?? 0),
+                  waitingListTile(
+                      controller.waitingPersons[0].getCustomers!,
+                      controller.waitingPersons[0].waitTime ?? 0,
+                      controller.waitingPersons[0]),
                   Padding(
                     padding: EdgeInsets.symmetric(vertical: 10.h),
                     child: const Divider(),
@@ -76,7 +78,7 @@ class WaitListUI extends GetView<WaitListUIController> {
                       WaitingPerson person =
                           controller.waitingPersons[index + 1];
                       return waitingListTile(
-                          person.getCustomers!, person.waitTime ?? 0);
+                          person.getCustomers!, person.waitTime ?? 0, person);
                     },
                   ),
                 ],
@@ -88,39 +90,60 @@ class WaitListUI extends GetView<WaitListUIController> {
     );
   }
 
-  Widget waitingListTile(GetCustomers waitingCustomer, int waitTime) {
-    return Row(
-      children: [
-        CachedNetworkImage(
-          imageUrl:
-              "${controller.preference.getBaseImageURL()}/${waitingCustomer.avatar ?? ""}",
-          placeholder: (context, url) =>
-              const Center(child: CircularProgressIndicator()),
-          errorWidget: (context, url, error) => CircleAvatar(
-              radius: 30,
-              backgroundImage: AssetImage(Assets.images.defaultProfile.path)),
-        ),
-        SizedBox(width: 10.w),
-        Expanded(
-          child: Text(
-            waitingCustomer.name ?? "",
-            style: AppTextStyle.textStyle16(fontColor: AppColors.darkBlue),
+  Widget waitingListTile(
+      GetCustomers waitingCustomer, int waitTime, WaitingPerson person) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10.h),
+      child: Row(
+        children: [
+          CachedNetworkImage(
+            imageUrl:
+                "${controller.preference.getBaseImageURL()}/${waitingCustomer.avatar ?? ""}",
+            placeholder: (context, url) =>
+                const Center(child: CircularProgressIndicator()),
+            errorWidget: (context, url, error) => CircleAvatar(
+                radius: 30,
+                backgroundImage: AssetImage(Assets.images.defaultProfile.path)),
           ),
-        ),
-        Row(
-          children: [
-            Assets.images.icChating.svg(),
-            SizedBox(
-              width: 8.w,
+          SizedBox(width: 10.w),
+          Expanded(
+            child: Text(
+              waitingCustomer.name ?? "",
+              style: AppTextStyle.textStyle16(fontColor: AppColors.darkBlue),
             ),
-            Text(
-              "$waitTime minutes",
-              style: AppTextStyle.textStyle16(
-                  fontWeight: FontWeight.w400, fontColor: AppColors.darkBlue),
-            ),
-          ],
-        ),
-      ],
+          ),
+          Row(
+            children: [
+              ImageBasedOnResponse(customer: person),
+              SizedBox(
+                width: 8.w,
+              ),
+              Text(
+                "$waitTime minutes",
+                style: AppTextStyle.textStyle16(
+                    fontWeight: FontWeight.w400, fontColor: AppColors.darkBlue),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
+  }
+}
+
+class ImageBasedOnResponse extends StatelessWidget {
+  const ImageBasedOnResponse({Key? key, required this.customer})
+      : super(key: key);
+
+  final WaitingPerson customer;
+  @override
+  Widget build(BuildContext context) {
+    if (customer.callType == 1) {
+      return Assets.images.icChating.svg();
+    }
+    if (customer.callType == 2) {
+      return Assets.svg.callIc1.svg();
+    }
+    return const SizedBox.shrink();
   }
 }
