5f33f3f865dab285d5a24107af8c9d7467c4c3ec
Jaydeep chatrola <jaydeep.chatrola@manektech.com>
2023-09-13 19:48:04 +0530

    Setup reverse duration using countdown time Fix popup issue in customer side for wait list Implement wallet recharge api Setup model class for model api Change flow for block user Block/unblock user using firebase

--- lib/common/waitlist_sheet.dart
+++ lib/common/waitlist_sheet.dart
@@ -9,12 +9,20 @@ import 'colors.dart';
 import 'custom_widgets.dart';
 
 class WaitList extends StatelessWidget {
-  final Function(String id,String name)? onAccept;
+  final Function(String id, String name)? onAccept;
   FirebaseDatabase database = FirebaseDatabase.instance;
   String? astroId;
   final controller = Get.find<LiveController>();
   bool? showNext = false;
-  WaitList({Key? key, this.onAccept, this.astroId,this.showNext}) : super(key: key);
+  Iterable<DataSnapshot> data = Iterable.empty();
+
+  WaitList(
+      {Key? key,
+      this.onAccept,
+      this.astroId,
+      this.showNext,
+      required this.data})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -25,7 +33,7 @@ class WaitList extends StatelessWidget {
         children: [
           GestureDetector(
             onTap: () {
-              Get.back();
+              //Get.back();
             },
             child: Container(
               padding: const EdgeInsets.all(15.0),
@@ -52,7 +60,8 @@ class WaitList extends StatelessWidget {
                 border: Border.all(color: AppColors.appYellowColour)),
             child: Column(
               children: [
-                FutureBuilder<DataSnapshot>(
+                buildWaitList(),
+                /*FutureBuilder<DataSnapshot>(
                     future: database.ref().child("live/$astroId/waitList").get(),
                     builder: (context, snapshot) {
                       if (snapshot.hasError) {
@@ -67,7 +76,7 @@ class WaitList extends StatelessWidget {
                       } else if (snapshot.data!.children.isNotEmpty) {
                         if(showNext!){
                           if (snapshot.data!.children.length - 1 == 0) {
-                            return SizedBox();
+                            return buildWaitList();
                           }
                         }
                         return Column(
@@ -136,9 +145,10 @@ class WaitList extends StatelessWidget {
                         );
                       }
                       return const SizedBox();
-                    }),
+                    }),*/
                 SizedBox(height: 20.h),
-                showNext! ? FutureBuilder<DataSnapshot>(
+                showNext!
+                    ? /*FutureBuilder<DataSnapshot>(
                     future:
                         database.ref().child("live/$astroId/waitList").get(),
                     builder: (context, snapshot) {
@@ -221,8 +231,10 @@ class WaitList extends StatelessWidget {
                           ],
                         );
                       }
-                      return SizedBox();
-                    }) : SizedBox(),
+                      return buildWaitList();
+                    })*/
+                    nextInLine()
+                    : buildWaitList(),
                 SizedBox(height: 20.h),
               ],
             ),
@@ -231,4 +243,338 @@ class WaitList extends StatelessWidget {
       ),
     );
   }
+
+  Widget buildWaitList() {
+    if (showNext!) {
+      if (data.length - 1 == 0) {
+        return SizedBox();
+      }
+    }
+    return Column(
+      children: [
+        SizedBox(height: 32.h),
+        Align(
+          alignment: Alignment.centerLeft,
+          child: Padding(
+            padding: EdgeInsets.only(left: 21.w),
+            child: CustomText("Waitlist",
+                fontSize: 20.sp, fontColor: AppColors.darkBlue),
+          ),
+        ),
+        SizedBox(height: 20.h),
+        SizedBox(
+          height: 100.h,
+          child: ListView.separated(
+            itemCount: data.length,
+            shrinkWrap: true,
+            padding: EdgeInsets.zero,
+            itemBuilder: (context, index) {
+              var item = data.toList()[index];
+              var value = item.value as Map;
+              return Container(
+                decoration: const BoxDecoration(color: AppColors.white),
+                child: Row(
+                  children: [
+                    SizedBox(width: 32.w),
+                    Container(
+                      width: 34.w,
+                      height: 34.h,
+                      clipBehavior: Clip.antiAlias,
+                      decoration: const BoxDecoration(shape: BoxShape.circle),
+                      child: Assets.images.avatar.svg(),
+                    ),
+                    SizedBox(width: 10.w),
+                    CustomText(value["name"],
+                        fontSize: 20.sp,
+                        fontWeight: FontWeight.w500,
+                        fontColor: AppColors.darkBlue),
+                    const Spacer(),
+                    const Icon(Icons.call, color: AppColors.darkBlue, size: 16),
+                    SizedBox(width: 10.w),
+                    CustomText(intToTimeLeft(value["duration"]),
+                        fontSize: 16.sp,
+                        fontWeight: FontWeight.w600,
+                        fontColor: AppColors.darkBlue),
+                    SizedBox(width: 32.w),
+                  ],
+                ),
+              );
+            },
+            separatorBuilder: (context, index) {
+              return SizedBox(height: 20.h);
+            },
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget nextInLine() {
+    var item = data.toList().first;
+    var value = item.value as Map;
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        SizedBox(height: 32.h),
+        Align(
+          alignment: Alignment.centerLeft,
+          child: Padding(
+            padding: EdgeInsets.only(left: 21.w),
+            child: CustomText("Next In Line",
+                fontSize: 20.sp, fontColor: AppColors.darkBlue),
+          ),
+        ),
+        SizedBox(height: 20.h),
+        Container(
+          decoration: const BoxDecoration(color: AppColors.white),
+          child: Row(
+            children: [
+              SizedBox(width: 32.w),
+              Container(
+                width: 34.w,
+                height: 34.h,
+                clipBehavior: Clip.antiAlias,
+                decoration: const BoxDecoration(shape: BoxShape.circle),
+                child: Assets.images.avatar.svg(),
+              ),
+              SizedBox(width: 10.w),
+              CustomText(value["name"] ?? "",
+                  fontSize: 20.sp,
+                  fontWeight: FontWeight.w500,
+                  fontColor: AppColors.darkBlue),
+              const Spacer(),
+              const Icon(Icons.call, color: AppColors.darkBlue, size: 16),
+              SizedBox(width: 10.w),
+              CustomText(intToTimeLeft(value["duration"]),
+                  fontSize: 16.sp,
+                  fontWeight: FontWeight.w600,
+                  fontColor: AppColors.darkBlue),
+              SizedBox(width: 32.w),
+            ],
+          ),
+        ),
+        SizedBox(height: 20.h),
+        Container(
+          margin: EdgeInsets.symmetric(horizontal: 20.w),
+          width: Get.width,
+          height: 56.h,
+          child: CustomButton(
+              onTap: () {
+                onAccept!(value["id"], value["name"]);
+              },
+              color: AppColors.appYellowColour,
+              radius: 28,
+              child: Center(
+                child: CustomText("Accept",
+                    fontSize: 16.sp,
+                    fontWeight: FontWeight.bold,
+                    fontColor: AppColors.brownColour),
+              )),
+        ),
+      ],
+    );
+  }
 }
+
+String intToTimeLeft(int value) {
+  int h, m, s;
+
+  h = value ~/ 3600;
+
+  m = ((value - h * 3600)) ~/ 60;
+
+  s = value - (h * 3600) - (m * 60);
+
+  String result = "$h:$m:$s";
+  if (h == 00) {
+    result = "$m m $s s";
+  } else {
+    result = "$h h $m m $s s";
+  }
+  return result;
+}
+/*
+Container(
+clipBehavior: Clip.antiAlias,
+decoration: BoxDecoration(
+borderRadius:
+const BorderRadius.vertical(top: Radius.circular(50.0)),
+color: AppColors.white,
+border: Border.all(color: AppColors.appYellowColour)),
+child: Column(
+children: [
+FutureBuilder<DataSnapshot>(
+future: database.ref().child("live/$astroId/waitList").get(),
+builder: (context, snapshot) {
+if (snapshot.hasError) {
+return const SizedBox();
+} else if (snapshot.connectionState ==
+ConnectionState.waiting) {
+return const Center(child: CircularProgressIndicator());
+} else if (!snapshot.hasData) {
+return const SizedBox();
+} else if (snapshot.data == null) {
+return const SizedBox();
+} else if (snapshot.data!.children.isNotEmpty) {
+if(showNext!){
+if (snapshot.data!.children.length - 1 == 0) {
+return SizedBox();
+}
+}
+return Column(
+children: [
+SizedBox(height: 32.h),
+Align(
+alignment: Alignment.centerLeft,
+child: Padding(
+padding: EdgeInsets.only(left: 21.w),
+child: CustomText("Waitlist",
+fontSize: 20.sp,
+fontColor: AppColors.darkBlue),
+),
+),
+SizedBox(height: 20.h),
+SizedBox(
+height: 100.h,
+child: ListView.separated(
+itemCount:
+snapshot.data!.children.length,
+shrinkWrap: true,
+padding: EdgeInsets.zero,
+itemBuilder: (context, index) {
+var item = snapshot.data!.children
+    .toList()[index];
+var data = item.value as Map;
+return Container(
+decoration: const BoxDecoration(
+color: AppColors.white),
+child: Row(
+children: [
+SizedBox(width: 32.w),
+Container(
+width: 34.w,
+height: 34.h,
+clipBehavior: Clip.antiAlias,
+decoration: const BoxDecoration(
+shape: BoxShape.circle),
+child: Assets.images.avatar.svg(),
+),
+SizedBox(width: 10.w),
+CustomText(data["name"],
+fontSize: 20.sp,
+fontWeight: FontWeight.w500,
+fontColor: AppColors.darkBlue),
+const Spacer(),
+const Icon(Icons.call,
+color: AppColors.darkBlue,
+size: 16),
+SizedBox(width: 10.w),
+CustomText("09 M 38 S",
+fontSize: 16.sp,
+fontWeight: FontWeight.w600,
+fontColor: AppColors.darkBlue),
+SizedBox(width: 32.w),
+],
+),
+);
+},
+separatorBuilder: (context, index) {
+return SizedBox(height: 20.h);
+},
+),
+),
+],
+);
+}
+return const SizedBox();
+}),
+SizedBox(height: 20.h),
+showNext! ? FutureBuilder<DataSnapshot>(
+future:
+database.ref().child("live/$astroId/waitList").get(),
+builder: (context, snapshot) {
+if (snapshot.hasError) {
+return const SizedBox();
+} else if (snapshot.connectionState ==
+ConnectionState.waiting) {
+return const Center(child: CircularProgressIndicator());
+} else if (!snapshot.hasData) {
+return const SizedBox();
+} else if (snapshot.data == null) {
+return const SizedBox();
+} else if (snapshot.data!.children.isNotEmpty) {
+var item = snapshot.data!.children.toList().first;
+var data = item.value as Map;
+return Column(
+mainAxisSize: MainAxisSize.min,
+children: [
+SizedBox(height: 32.h),
+Align(
+alignment: Alignment.centerLeft,
+child: Padding(
+padding: EdgeInsets.only(left: 21.w),
+child: CustomText("Next In Line",
+fontSize: 20.sp,
+fontColor: AppColors.darkBlue),
+),
+),
+SizedBox(height: 20.h),
+Container(
+decoration:
+const BoxDecoration(color: AppColors.white),
+child: Row(
+children: [
+SizedBox(width: 32.w),
+Container(
+width: 34.w,
+height: 34.h,
+clipBehavior: Clip.antiAlias,
+decoration: const BoxDecoration(
+shape: BoxShape.circle),
+child: Assets.images.avatar.svg(),
+),
+SizedBox(width: 10.w),
+CustomText(
+data["name"] ?? "",
+fontSize: 20.sp,
+fontWeight: FontWeight.w500,
+fontColor: AppColors.darkBlue),
+const Spacer(),
+const Icon(Icons.call,
+color: AppColors.darkBlue, size: 16),
+SizedBox(width: 10.w),
+CustomText("09 M 38 S",
+fontSize: 16.sp,
+fontWeight: FontWeight.w600,
+fontColor: AppColors.darkBlue),
+SizedBox(width: 32.w),
+],
+),
+),
+SizedBox(height: 20.h),
+Container(
+margin: EdgeInsets.symmetric(horizontal: 20.w),
+width: Get.width,
+height: 56.h,
+child: CustomButton(
+onTap: () {
+onAccept!(data["id"],data["name"]);
+},
+color: AppColors.appYellowColour,
+radius: 28,
+child: Center(
+child: CustomText("Accept",
+fontSize: 16.sp,
+fontWeight: FontWeight.bold,
+fontColor: AppColors.brownColour),
+)),
+),
+],
+);
+}
+return SizedBox();
+}) : SizedBox(),
+SizedBox(height: 20.h),
+],
+),
+),*/
