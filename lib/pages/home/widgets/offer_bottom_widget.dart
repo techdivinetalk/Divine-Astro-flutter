d585d8abcce45a7c3ca329cf7154d82a6ff810ac
Raj <rajy3512@gmail.com>
2024-03-15 20:06:27 +0530

    Update code bottomSheet error E-commerce

--- lib/pages/home/widgets/offer_bottom_widget.dart
+++ lib/pages/home/widgets/offer_bottom_widget.dart
@@ -20,15 +20,15 @@ class CustomInfoWidget extends StatelessWidget {
         children: [
           Container(
             height: 70.h,
-            padding: EdgeInsets.only(top: 15, bottom: 15, left: 35.sp, right: 35.sp),
+            padding: EdgeInsets.symmetric(vertical: 15, horizontal: 35.sp),
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(15),
               border: Border.all(color: Colors.black),
             ),
-            child: Expanded(
-              child: Row(
-                children: [
-                  Text(
+            child: Row(
+              children: [
+                Expanded(
+                  child: Text(
                     text,
                     textAlign: TextAlign.start,
                     maxLines: 3,
@@ -37,9 +37,9 @@ class CustomInfoWidget extends StatelessWidget {
                       fontSize: 14.sp,
                       fontWeight: FontWeight.w500,
                     ),
-                  )
-                ],
-              ),
+                  ),
+                ),
+              ],
             ),
           ),
           Positioned(
