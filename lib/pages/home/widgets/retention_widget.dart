f8dd01f12627c7867108cc08f96ba47cb96cb04f
sahildivine <Sahil.umretiya@divinetalk.in>
2024-03-19 16:23:41 +0530

    work of 19 march

--- lib/pages/home/widgets/retention_widget.dart
+++ lib/pages/home/widgets/retention_widget.dart
@@ -13,59 +13,65 @@ class RetentionWidget extends StatelessWidget {
   final int? minimumRepurchaseRate;
   final VoidCallback? onTap;
 
-  const RetentionWidget({super.key,
-  this.onTap,
+  const RetentionWidget({
+    super.key,
+    this.onTap,
     this.borderColor,
     this.bottomColor,
-    this.bottomTextColor, this.title, this.subTitle, this.child, this.minimumRetention, this.minimumRepurchaseRate,
+    this.bottomTextColor,
+    this.title,
+    this.subTitle,
+    this.child,
+    this.minimumRetention,
+    this.minimumRepurchaseRate,
   });
 
   @override
   Widget build(BuildContext context) {
-    Color bottomColor = minimumRetention == minimumRepurchaseRate ? appColors.green : appColors.red;
     return GestureDetector(
       onTap: onTap,
       child: Container(
         height: 60,
         decoration: BoxDecoration(
           color: appColors.transparent,
-          border: Border.all(color: borderColor ?? bottomColor),
+          border: Border.all(color: borderColor ?? appColors.red),
           borderRadius: BorderRadius.circular(10),
         ),
-        child:child ?? Column(
-          children: [
-            Expanded(
-              child: Center(
-                child: Text(
-                  title ?? "",
-                  style: AppTextStyle.textStyle9(
-                    fontWeight: FontWeight.w400,
-                  ),
-                ),
-              ),
-            ),
-            Expanded(
-              child: Container(
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  color: bottomColor,
-                  borderRadius: const BorderRadius.vertical(
-                    bottom: Radius.circular(10),
-                  ),
-                ),
-                child: Center(
-                  child: Text(
-                    subTitle ?? "",
-                    style: AppTextStyle.textStyle9(
-                      fontWeight: FontWeight.w400,
-                      fontColor: bottomTextColor ?? appColors.white,
+        child: child ??
+            Column(
+              children: [
+                Expanded(
+                  child: Center(
+                    child: Text(
+                      title ?? "",
+                      style: AppTextStyle.textStyle9(
+                        fontWeight: FontWeight.w400,
+                      ),
                     ),
                   ),
                 ),
-              ),
+                Expanded(
+                  child: Container(
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: borderColor,
+                      borderRadius: const BorderRadius.vertical(
+                        bottom: Radius.circular(10),
+                      ),
+                    ),
+                    child: Center(
+                      child: Text(
+                        subTitle ?? "",
+                        style: AppTextStyle.textStyle9(
+                          fontWeight: FontWeight.w400,
+                          fontColor: bottomTextColor ?? appColors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
       ),
     );
   }
