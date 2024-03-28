ce7cdb33f3cd8b673f88e4ae8e46fd944b16336f
Raj <inforajy@gmail.com>
2024-03-28 17:39:46 +0530

    Upadte Code Ui

--- lib/pages/home/widgets/retention_widget.dart
+++ lib/pages/home/widgets/retention_widget.dart
@@ -3,6 +3,7 @@ import 'package:divine_astrologer/common/colors.dart';
 import 'package:divine_astrologer/common/custom_widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class RetentionWidget extends StatelessWidget {
   final Color? borderColor;
@@ -11,7 +12,6 @@ class RetentionWidget extends StatelessWidget {
   final String? title;
   final String? subTitle;
   final Widget? child;
-
   final VoidCallback? onTap;
 
   const RetentionWidget({
@@ -23,62 +23,65 @@ class RetentionWidget extends StatelessWidget {
     this.title,
     this.subTitle,
     this.child,
-
-
   });
 
   @override
   Widget build(BuildContext context) {
+    final titleLines = (title ?? "").split('\n').length;
+    final subTitleLines = (subTitle ?? "").split('\n').length;
+    final totalLines = titleLines + subTitleLines;
+
+    final height = totalLines > 2 ? 70.h : 50.h;
 
     return GestureDetector(
       onTap: onTap,
       child: Container(
-        height: 50.h,
+        height: height,
         width: MediaQuery.of(context).size.width,
         decoration: BoxDecoration(
           color: appColors.transparent,
           border: Border.all(color: borderColor ?? Colors.transparent),
           borderRadius: BorderRadius.circular(10),
         ),
-        child: child ?? Column(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Expanded(
-              child: Center(
-                child: CustomText(
-                  title ?? "",
-                  fontWeight: FontWeight.w400,
-                  textAlign: TextAlign.center,
-                  fontSize: 9.sp,
-                  maxLines: 2,
-                  fontColor: appColors.textColor,
-                ),
-              ),
-            ),
-            Expanded(
-              child: Container(
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  color:  borderColor ?? Colors.transparent,
-                  borderRadius: const BorderRadius.vertical(
-                    bottom: Radius.circular(10),
+        child: child ??
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: Center(
+                    child: CustomText(
+                      title ?? "",
+                      fontWeight: FontWeight.w400,
+                      textAlign: TextAlign.center,
+                      fontSize: 9.sp,
+                      maxLines: 2,
+                      fontColor: appColors.textColor,
+                    ),
                   ),
                 ),
-                child: Center(
-                  child: CustomText(
-                    subTitle ?? "",
-                    fontWeight: FontWeight.w400,
-                    textAlign: TextAlign.center,
-                    fontSize: 8.5.sp,
-                    maxLines: 2,
-                    fontColor: bottomTextColor ?? appColors.white,
+                Expanded(
+                  child: Container(
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: borderColor ?? Colors.transparent,
+                      borderRadius: const BorderRadius.vertical(
+                        bottom: Radius.circular(10),
+                      ),
+                    ),
+                    child: Center(
+                      child: CustomText(
+                        subTitle ?? "",
+                        fontWeight: FontWeight.w400,
+                        textAlign: TextAlign.center,
+                        fontSize: 8.5.sp,
+                        maxLines: 2,
+                        fontColor: bottomTextColor ?? appColors.white,
+                      ),
+                    ),
                   ),
                 ),
-              ),
+              ],
             ),
-          ],
-        ),
       ),
     );
   }
