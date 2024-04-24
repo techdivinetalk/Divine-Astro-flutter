df2ba66a5c52f34ec2c75252b15c254985098480
Shazam <Shaikhzameer837@gmail.com>
2024-04-24 12:33:51 +0530

    sms permission removed

--- lib/screens/otp_verification/otp_verification_controller.dart
+++ lib/screens/otp_verification/otp_verification_controller.dart
@@ -8,8 +8,6 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
-import 'package:sms_autofill/sms_autofill.dart';
-
 import '../../common/app_exception.dart';
 import '../../common/colors.dart';
 import '../../common/common_functions.dart';
@@ -21,7 +19,7 @@ import '../../model/verify_otp.dart';
 import '../../repository/user_repository.dart';
 
 //var globalToken = "";
-class OtpVerificationController extends GetxController with CodeAutoFill {
+class OtpVerificationController extends GetxController {
   OtpVerificationController(this.userRepository);
 
   final UserRepository userRepository;
@@ -44,7 +42,7 @@ class OtpVerificationController extends GetxController with CodeAutoFill {
 
   @override
   void onReady() async {
-    listenForCode();
+  //  listenForCode();
     var arguments = Get.arguments;
     if (arguments != null) {
       var args = arguments as List;
@@ -283,13 +281,12 @@ class OtpVerificationController extends GetxController with CodeAutoFill {
 
   String? otpCode;
 
-  @override
-  void codeUpdated() {
-    otpCode = code!;
-    pinController.text = code ?? "";
-    if (pinController.text.isNotEmpty) {
-      verifyOtp();
-    }
-    update();
-  }
+  // void codeUpdated() {
+  //   otpCode = code!;
+  //   pinController.text = code ?? "";
+  //   if (pinController.text.isNotEmpty) {
+  //     verifyOtp();
+  //   }
+  //   update();
+  // }
 }
