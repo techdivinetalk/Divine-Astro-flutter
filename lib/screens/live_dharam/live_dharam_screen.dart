71bcc25262c56a03ab846244b833b0e89241639b
sahil_umretiya <sahilumretiya07@gmail.com>
2024-05-02 17:31:32 +0530

    worked on notification

--- lib/screens/live_dharam/live_dharam_screen.dart
+++ lib/screens/live_dharam/live_dharam_screen.dart
@@ -738,6 +738,7 @@ class _LivePage extends State<LiveDharamScreen>
                     userName: _controller.userName,
                     liveID: _controller.liveId,
                     config: streamingConfig
+                        ..video = ZegoUIKitVideoConfig.preset1080P()
                       ..preview.showPreviewForHost = false
                       ..audioVideoView.isVideoMirror = false
                       ..maxCoHostCount = 1
