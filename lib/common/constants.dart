c1f1dbd0cfb790bc7d030d4853563e1ca590af29
Nikunj <nikunj@divinetalk.in>
2024-05-29 09:59:44 +0530

    - Code update minor changes.

--- lib/common/constants.dart
+++ lib/common/constants.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/foundation.dart';
 
-class Constants{
-  static bool isTestingMode=false; // TODO: Nikunj: Please check this condition before live the app.
-  static bool isUploadMode=true; // TODO: Nikunj: Please check this condition before live the app.
-  static bool isDebugMode=false; // TODO: Nikunj: Please check this condition before live the app.
+class Constants {
+  static bool isTestingMode =
+      false; // TODO: Nikunj: Please check this condition before live the app.
+  static bool isUploadMode =
+      true; // TODO: Nikunj: Please check this condition before live the app.
+  static bool isDebugMode =
+      false; // TODO: Nikunj: Please check this condition before live the app.
 }
