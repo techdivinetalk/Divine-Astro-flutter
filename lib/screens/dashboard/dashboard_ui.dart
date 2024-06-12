ccf997a1ced531503047037699b1a597a2d9658e
sahil_umretiya <sahilumretiya07@gmail.com>
2024-06-12 17:56:04 +0530

    latest code

--- lib/screens/dashboard/dashboard_ui.dart
+++ lib/screens/dashboard/dashboard_ui.dart
@@ -325,12 +325,12 @@ class DashboardScreen extends GetView<DashboardController> {
             Expanded(
               flex: 6,
               child: Wrap(direction: Axis.horizontal, children: [
-                CustomText('Your Customer ', fontSize: 10.sp),
+                CustomText('You have request from ', fontSize: 10.sp),
                 CustomText(chatOrderData!.getCustomers.name,
                     fontSize: 10.sp,
                     fontColor: appColors.brown,
                     fontWeight: FontWeight.w700),
-                CustomText(' already joined', fontSize: 10.sp),
+                CustomText(' accept it', fontSize: 10.sp),
                 SizedBox(width: 8.w),
               ]),
             ),
