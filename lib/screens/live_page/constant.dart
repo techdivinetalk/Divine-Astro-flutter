1f615f0f5ad2f840451d94c5344023234d460e92
sahil_umretiya <sahilumretiya07@gmail.com>
2024-06-12 18:14:10 +0530

    latest code

--- lib/screens/live_page/constant.dart
+++ lib/screens/live_page/constant.dart
@@ -19,7 +19,7 @@ const yourAppSign =
 
 const yourServerSecret = '89ceddc6c59909af326ddb7209cb1c16';
 RxList assistChatNewMsg = [].obs;
-const livePath = kDebugMode  ? 'liveTest':"live";
+const livePath = "live";
 const liveCountPath =  "liveCount";
 RxInt chatAssistantCurrentUserId = 0.obs;
 RxInt walletBalance = 0.obs;
