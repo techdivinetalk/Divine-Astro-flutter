cc229ebc704a40a9312f2b8ead1c5c2b5331fd7e
sahil-developer <sahil.viradiya@badibahen.in>
2024-06-13 23:58:31 +0530

    Registration screen ui Done

--- lib/pages/new_registration/new_registration_screen.dart
+++ lib/pages/new_registration/new_registration_screen.dart
@@ -2,72 +2,228 @@ import 'package:animated_custom_dropdown/custom_dropdown.dart';
 import 'package:divine_astrologer/common/app_textstyle.dart';
 import 'package:divine_astrologer/common/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 
-const List<String> _list = [
-  'Law Earning',
-  'Health Issues',
-  'Better Opportunity',
-  'Personal Reason',
-  'Other',
-];
+import 'new_registration_controller.dart';
 
-class NewRegstrationScreen extends StatelessWidget {
-  const NewRegstrationScreen({super.key});
+class NewRegstrationScreen extends GetView<NewRegistrationController> {
+  NewRegstrationScreen({super.key});
+
+  List<String> _list = [
+    'Law Earning',
+    'Health Issues',
+    'Better Opportunity',
+    'Personal Reason',
+    'Other',
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: appbarSmall1(context, "Resignation".tr),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            margin: EdgeInsets.symmetric(horizontal: 16),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(12),
-              border: Border.all(color: AppColors().greyColor2)
-            ),
-            child: CustomDropdown<String>(
-              // decoration: CustomDropdownDecoration(
-              //   expandedBorder: Border.all(color: AppColors().blackColor),
-              // ),
-              hintText: 'Select job role',
-              items: _list,
-              initialItem: _list[0],
-              onChanged: (value) {
-                print('changing value to: $value');
-              },
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 16,vertical: 8),
-            child: RichText(
-              text: TextSpan(
-                children: [
-                  TextSpan(
-                    text: 'Having Issues?',
-                    style: AppTextStyle.textStyle14(),
-                      // Single tapped.
-                  ),
-                  TextSpan(
-                      text: ' Call Now!',
-                    style: AppTextStyle.textStyle14(
-                      fontColor: AppColors().red,
-                    ),
-
-                        // Double tapped.
-                  ),
-                  TextSpan(
-                    text: ' Long press',
-                      // Long Pressed.
-                  ),
-                ],
+    return GetBuilder<NewRegistrationController>(
+      init: NewRegistrationController(),
+      initState: (_) {},
+      builder: (_) {
+        return Scaffold(
+          appBar: appbarSmall1(context, "Resignation".tr),
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 16),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    border: Border.all(color: AppColors().greyColor2)),
+                child: CustomDropdown<String>(
+                  decoration: CustomDropdownDecoration(
+                      headerStyle: AppTextStyle.textStyle16(),
+                      listItemStyle: AppTextStyle.textStyle16()),
+                  closedHeaderPadding:
+                      EdgeInsets.symmetric(horizontal: 16, vertical: 0),
+                  hintText: 'Select job role',
+                  items: _list,
+                  initialItem: _list[0],
+                  onChanged: (value) {
+                    controller.selectedValue(value);
+                    print("S====> ${controller.selectedValue.value}");
+                  },
+                ),
               ),
-            ),
-          )
-        ],
-      ),
+              Obx(() {
+                return controller.selectedValue.value == "Other"
+                    ? Padding(
+                        padding: EdgeInsets.only(top: 10, right: 16, left: 16),
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            hintText: "Type Your Reason here",
+                            contentPadding: EdgeInsets.symmetric(
+                                horizontal: 16, vertical: 0),
+                            hintStyle: AppTextStyle.textStyle16(),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(12),
+                              borderSide:
+                                  BorderSide(color: AppColors().greyColor2),
+                            ),
+                            disabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(12),
+                              borderSide:
+                                  BorderSide(color: AppColors().greyColor2),
+                            ),
+                            errorBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(12),
+                              borderSide:
+                                  BorderSide(color: AppColors().greyColor2),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(12),
+                              borderSide:
+                                  BorderSide(color: AppColors().greyColor2),
+                            ),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(12),
+                              borderSide:
+                                  BorderSide(color: AppColors().greyColor2),
+                            ),
+                          ),
+                        ),
+                      )
+                    : SizedBox();
+              }),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+                child: RichText(
+                  text: TextSpan(
+                    children: [
+                      TextSpan(
+                        text: 'Having Issues? ',
+                        style: AppTextStyle.textStyle14(),
+                        // Single tapped.
+                      ),
+                      TextSpan(
+                        text: 'Call Now!',
+                        style: AppTextStyle.textStyle14(
+                          fontColor: AppColors().red,
+                        ).copyWith(
+                          decoration: TextDecoration.underline,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Spacer(),
+              Obx(
+                () => controller.showRichText.value
+                    ? Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          RichText(
+                            text: TextSpan(
+                              children: [
+                                TextSpan(
+                                  text: 'Resignation Status - ',
+                                  style: AppTextStyle.textStyle16(
+                                    fontWeight: FontWeight.w500,
+                                  ).copyWith(fontFamily: "Metropolis"),
+                                  // Single tapped.
+                                ),
+                                TextSpan(
+                                  text: 'Pending',
+                                  style: AppTextStyle.textStyle16(
+                                    fontWeight: FontWeight.w500,
+                                    fontColor: AppColors().red,
+                                  ).copyWith(fontFamily: "Metropolis"),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(
+                                left: 16, right: 16, top: 15, bottom: 20),
+                            height: 48,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                border: Border.all(color: AppColors().red)),
+                            child: Material(
+                              color: Colors.transparent,
+                              child: InkWell(
+                                borderRadius: BorderRadius.circular(10),
+                                splashColor: Colors.red.withOpacity(0.5),
+                                highlightColor: Colors.transparent,
+                                onTap: () {
+                                  controller.showRichText(false);
+                                },
+                                child: Center(
+                                  child: Text(
+                                    "Cancel Resignation",
+                                    style: AppTextStyle.textStyle16(
+                                      fontWeight: FontWeight.w500,
+                                      fontColor: AppColors().red,
+                                    ).copyWith(fontFamily: "Metropolis"),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      )
+                    : Container(
+                        margin:
+                            EdgeInsets.symmetric(vertical: 20, horizontal: 16),
+                        height: 48,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            border: Border.all(color: AppColors().red)),
+                        child: Material(
+                          color: Colors.transparent,
+                          child: InkWell(
+                            borderRadius: BorderRadius.circular(10),
+                            splashColor: Colors.red.withOpacity(0.5),
+                            highlightColor: Colors.transparent,
+                            onTap: () {
+                              showDialog(
+                                context: context,
+                                builder: (BuildContext context) {
+                                  Future.delayed(Duration(seconds: 2), () {
+                                    Navigator.of(context).pop(true);
+                                    controller.showRichText(true);
+                                  });
+                                  return AlertDialog(
+                                    alignment: Alignment.center,
+                                    icon: SvgPicture.asset(
+                                        "assets/svg/righticon.svg"),
+                                    title: Text('Resignation Submitted',
+                                        style: AppTextStyle.textStyle16()),
+                                    content: Text(
+                                      'Our support team will get in touch\nwith you soon.',
+                                      style: AppTextStyle.textStyle14(
+                                        fontWeight: FontWeight.w400,
+                                        fontColor: AppColors().greyColor2,
+                                      ).copyWith(fontFamily: "Metropolis"),
+                                      textAlign: TextAlign.center,
+                                    ),
+                                  );
+                                },
+                              );
+                            },
+                            child: Center(
+                              child: Text(
+                                "Submit Resignation",
+                                style: AppTextStyle.textStyle16(
+                                  fontWeight: FontWeight.w500,
+                                  fontColor: AppColors().red,
+                                ).copyWith(fontFamily: "Metropolis"),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+              )
+            ],
+          ),
+        );
+      },
     );
   }
 }
@@ -98,3 +254,24 @@ appbarSmall1(BuildContext context, String title,
     // centerTitle: true,
   );
 }
+
+// MyDropDown(
+//   shape: RoundedRectangleBorder(
+//       side: const BorderSide(color: colorBlack, width: 1),
+//       borderRadius: constants.borderRadius),
+//   color: colorBgLight,
+//   borderWidth: 0.0,
+//   icon: const Icon(
+//     Icons.keyboard_arrow_down_rounded,
+//     size: 30,
+//     color: colorBlack,
+//   ),
+//   hintText: 'Select Subject',
+//   defaultValue:  controller.selectedSubject.value,
+//   style: AppTextStyle.smallText.copyWith(
+//       color: Colors.black45, fontWeight: FontWeight.bold),
+//   onChange: (value) {
+//     controller.selectedSubject(value);
+//   },
+//   array: _list,
+// ),
