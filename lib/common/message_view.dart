a6c7b076544165e4d6105f5102dfcc92aca9035c
Bhawani Shankar <bhawani2505@gmail.com>
2024-07-29 15:23:48 +0530

    image not downloading fixed

--- lib/common/message_view.dart
+++ lib/common/message_view.dart
@@ -24,6 +24,7 @@ import 'package:fluttertoast/fluttertoast.dart';
 import 'package:get/get.dart';
 import 'package:intl/intl.dart';
 import 'package:voice_message_package/voice_message_package.dart';
+import 'package:path_provider/path_provider.dart';
 
 import '../screens/chat_assistance/chat_message/widgets/product/pooja/widgets/custom_widget/pooja_common_list.dart';
 
@@ -899,111 +900,121 @@ class MessageView extends StatelessWidget {
                           ),
                         ],
                       )
-                : chatDetail.downloadedPath == "" ||
-                        chatDetail.downloadedPath == null
-                    ? Stack(
-                        alignment: Alignment.center,
-                        children: [
-                          ClipRRect(
-                            borderRadius: BorderRadius.circular(10.0.sp),
-                            child: ImageFiltered(
-                              imageFilter:
-                                  ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
-                              child: Image.network(
-                                "${chatDetail.message}",
-                                fit: BoxFit.cover,
-                                height: 200.h,
-                                width: 200.h,
-                              ),
-                            ),
-                          ),
-                          if (chatDetail.message != null)
-                            InkWell(
-                              onTap: () {
-                                chatController.downloadImage(
-                                  fileName: image,
-                                  chatDetail: chatDetail,
-                                  index: index,
-                                );
-                              },
-                              child: Container(
-                                padding: const EdgeInsets.all(10),
-                                decoration: BoxDecoration(
-                                  color: appColors.darkBlue.withOpacity(0.3),
-                                  shape: BoxShape.circle,
-                                ),
-                                child: Icon(
-                                  Icons.download_rounded,
-                                  color: appColors.white,
-                                ),
-                              ),
-                            ),
-                          Positioned(
-                            bottom: 0,
-                            right: 0,
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.end,
+                : FutureBuilder(
+                    future: getDownloadedImagePath(chatDetail.id ?? 0),
+                    builder: (context, snapshot) {
+                      return (chatDetail.downloadedPath == "" ||
+                                  chatDetail.downloadedPath == null) &&
+                              snapshot.data == null
+                          ? Stack(
+                              alignment: Alignment.center,
                               children: [
-                                Container(
-                                  padding: const EdgeInsets.symmetric(
-                                    horizontal: 6,
-                                  ).copyWith(left: 10),
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.only(
-                                      bottomRight: Radius.circular(10.r),
-                                    ),
-                                    gradient: LinearGradient(
-                                      colors: [
-                                        appColors.darkBlue.withOpacity(0.0),
-                                        appColors.darkBlue.withOpacity(0.0),
-                                        appColors.darkBlue.withOpacity(0.5),
-                                      ],
-                                      begin: Alignment.topLeft,
-                                      end: Alignment.bottomCenter,
-                                    ),
-                                  ),
-                                  child: Text(
-                                    messageDateTime(
-                                        int.parse("${chatDetail.time ?? "0"}")),
-                                    style: AppTextStyle.textStyle10(
-                                      fontColor: appColors.white,
-                                    ),
-                                  ),
-                                ),
-                                SizedBox(width: 8.w),
-                              ],
-                            ),
-                          ),
-                        ],
-                      )
-                    : InkWell(
-                        onTap: () {
-                          Get.toNamed(RouteName.imagePreviewUi,
-                              arguments: chatDetail.message != null
-                                  ? "${chatDetail.message}"
-                                  : "${pref.getAmazonUrl()}${chatDetail.awsUrl}");
-                        },
-                        child: Stack(
-                          children: [
-                            ClipRRect(
-                              borderRadius: BorderRadius.circular(8.0.r),
-                              child: chatDetail.message != null
-                                  ? Image.network(
+                                ClipRRect(
+                                  borderRadius: BorderRadius.circular(10.0.sp),
+                                  child: ImageFiltered(
+                                    imageFilter: ImageFilter.blur(
+                                        sigmaX: 5.0, sigmaY: 5.0),
+                                    child: Image.network(
                                       "${chatDetail.message}",
                                       fit: BoxFit.cover,
                                       height: 200.h,
                                       width: 200.h,
-                                    )
-                                  : Image.file(
-                                      File(chatDetail.downloadedPath ?? ""),
-                                      fit: BoxFit.cover,
-                                      height: 200.h,
-                                      width: 200.h,
                                     ),
-                            ),
-                          ],
-                        ),
-                      ),
+                                  ),
+                                ),
+                                if (chatDetail.message != null)
+                                  InkWell(
+                                    onTap: () {
+                                      chatController.downloadImage(
+                                        fileName: image,
+                                        chatDetail: chatDetail,
+                                        index: index,
+                                      );
+                                    },
+                                    child: Container(
+                                      padding: const EdgeInsets.all(10),
+                                      decoration: BoxDecoration(
+                                        color:
+                                            appColors.darkBlue.withOpacity(0.3),
+                                        shape: BoxShape.circle,
+                                      ),
+                                      child: Icon(
+                                        Icons.download_rounded,
+                                        color: appColors.white,
+                                      ),
+                                    ),
+                                  ),
+                                Positioned(
+                                  bottom: 0,
+                                  right: 0,
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.end,
+                                    children: [
+                                      Container(
+                                        padding: const EdgeInsets.symmetric(
+                                          horizontal: 6,
+                                        ).copyWith(left: 10),
+                                        decoration: BoxDecoration(
+                                          borderRadius: BorderRadius.only(
+                                            bottomRight: Radius.circular(10.r),
+                                          ),
+                                          gradient: LinearGradient(
+                                            colors: [
+                                              appColors.darkBlue
+                                                  .withOpacity(0.0),
+                                              appColors.darkBlue
+                                                  .withOpacity(0.0),
+                                              appColors.darkBlue
+                                                  .withOpacity(0.5),
+                                            ],
+                                            begin: Alignment.topLeft,
+                                            end: Alignment.bottomCenter,
+                                          ),
+                                        ),
+                                        child: Text(
+                                          messageDateTime(int.parse(
+                                              "${chatDetail.time ?? "0"}")),
+                                          style: AppTextStyle.textStyle10(
+                                            fontColor: appColors.white,
+                                          ),
+                                        ),
+                                      ),
+                                      SizedBox(width: 8.w),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            )
+                          : InkWell(
+                              onTap: () {
+                                Get.toNamed(RouteName.imagePreviewUi,
+                                    arguments: chatDetail.message != null
+                                        ? "${chatDetail.message}"
+                                        : "${pref.getAmazonUrl()}${chatDetail.awsUrl}");
+                              },
+                              child: Stack(
+                                children: [
+                                  ClipRRect(
+                                    borderRadius: BorderRadius.circular(8.0.r),
+                                    child: chatDetail.message != null
+                                        ? Image.network(
+                                            "${chatDetail.message}",
+                                            fit: BoxFit.cover,
+                                            height: 200.h,
+                                            width: 200.h,
+                                          )
+                                        : Image.file(
+                                            File(chatDetail.downloadedPath ??
+                                                ""),
+                                            fit: BoxFit.cover,
+                                            height: 200.h,
+                                            width: 200.h,
+                                          ),
+                                  ),
+                                ],
+                              ),
+                            );
+                    }),
           ),
         ],
       ),
@@ -1209,5 +1220,20 @@ class MessageView extends StatelessWidget {
         child: Text("unreadMessages".tr),
       ),
     );
+  }
+}
+
+/// ------------------ check if file exists  ----------------------- ///
+Future<String?> getDownloadedImagePath(
+  int chatDetailId,
+) async {
+  var documentDirectory = await getApplicationDocumentsDirectory();
+  var filePathAndName = "${documentDirectory.path}/images/$chatDetailId.jpg";
+
+  // Check if the file already exists
+  if (await File(filePathAndName).exists()) {
+    return filePathAndName;
+  } else {
+    return null;
   }
 }
