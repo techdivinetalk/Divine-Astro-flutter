30c63c8fbe304f41a1baeb24688bdf320f0df6df
raj <bamalwaraj@gmail.com>
2024-08-12 21:06:07 +0530

    Issues Solved

--- lib/pages/home/home_ui.dart
+++ lib/pages/home/home_ui.dart
@@ -1934,23 +1934,23 @@ class HomeUI extends GetView<HomeController> {
                               Get.toNamed(RouteName.referAstrologer);
                             }),
                         trainingVideoWidget(controller: controller),
-                        Obx(() {
-                          return showDailyLive.value.toString() == "1"
-                              ? Visibility(
-                                  visible: controller.isLiveMonitor.value != 1,
-                                  child: Padding(
-                                    padding: const EdgeInsets.only(
-                                        top: 6, bottom: 6),
-                                    child: Column(
-                                      children: [
-                                        liveWidgetUpdated(),
-                                        // SizedBox(height: 10.h),
-                                      ],
-                                    ),
-                                  ),
-                                )
-                              : SizedBox();
-                        }),
+                        // Obx(() {
+                        //   return showDailyLive.value.toString() == "1"
+                        //       ? Visibility(
+                        //           visible: controller.isLiveMonitor.value != 1,
+                        //           child: Padding(
+                        //             padding: const EdgeInsets.only(
+                        //                 top: 6, bottom: 6),
+                        //             child: Column(
+                        //               children: [
+                        //                 liveWidgetUpdated(),
+                        //                 // SizedBox(height: 10.h),
+                        //               ],
+                        //             ),
+                        //           ),
+                        //         )
+                        //       : SizedBox();
+                        // }),
                         (controller.customerDetailsResponse == null ||
                                 controller
                                     .customerDetailsResponse!.data.isEmpty)
