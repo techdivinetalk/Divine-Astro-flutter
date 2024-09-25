55f2189b11e5ab12daaad8a2cbe709e549a36b13
raj <bamalwaraj@gmail.com>
2024-09-25 16:00:35 +0530

    Message Template default template solve

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -50,20 +50,20 @@ class DefaultFirebaseOptions {
   }
 
   static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyD6TwQ1az8IU6mmhKZoeLa4kfdQa0BrbQY',
-    appId: '1:764246952124:android:dba9eba1fb1f4d806fe8b9',
-    messagingSenderId: '764246952124',
-    projectId: 'divine2-0-uat',
-    databaseURL:
-        "https://divine2-0-uat-default-rtdb.firebaseio.com", // 'https://divine-live-f7f79-default-rtdb.asia-southeast1.firebasedatabase.app',
-    storageBucket: 'divine2-0-uat.appspot.com',
-    // apiKey: 'AIzaSyC06RdHRB5jW2ri9DSbrd-fHh77yHaXJ1o',
-    // appId: '1:507681862980:android:abf46c3e015ebea1e51311',
-    // messagingSenderId: '507681862980',
-    // projectId: 'divine-live-f7f79',
+    // apiKey: 'AIzaSyD6TwQ1az8IU6mmhKZoeLa4kfdQa0BrbQY',
+    // appId: '1:764246952124:android:dba9eba1fb1f4d806fe8b9',
+    // messagingSenderId: '764246952124',
+    // projectId: 'divine2-0-uat',
     // databaseURL:
-    //     'https://divine-live-f7f79-default-rtdb.asia-southeast1.firebasedatabase.app',
-    // storageBucket: 'divine-live-f7f79.appspot.com',
+    //     "https://divine2-0-uat-default-rtdb.firebaseio.com", // 'https://divine-live-f7f79-default-rtdb.asia-southeast1.firebasedatabase.app',
+    // storageBucket: 'divine2-0-uat.appspot.com',
+    apiKey: 'AIzaSyC06RdHRB5jW2ri9DSbrd-fHh77yHaXJ1o',
+    appId: '1:507681862980:android:abf46c3e015ebea1e51311',
+    messagingSenderId: '507681862980',
+    projectId: 'divine-live-f7f79',
+    databaseURL:
+        'https://divine-live-f7f79-default-rtdb.asia-southeast1.firebasedatabase.app',
+    storageBucket: 'divine-live-f7f79.appspot.com',
   );
   static const FirebaseOptions ios = FirebaseOptions(
     apiKey: 'AIzaSyB8qPgVe6EJ2hdsp-FTVBVbP3oOQOK47xs',
