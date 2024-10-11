9180ec4f9077c5ee0d5291d172b69d49e7747c67
raj <bamalwaraj@gmail.com>
2024-10-11 17:43:31 +0530

    Issues solved

--- lib/screens/chat_assistance/chat_message/chat_assistant_message_ui.dart
+++ lib/screens/chat_assistance/chat_message/chat_assistant_message_ui.dart
@@ -225,7 +225,7 @@ class _ChatMessageSupportUIState extends State<ChatMessageSupportUI> {
           ),
         ),
         actions: [
-          show_call_on_assistant.value.toString() == "0"
+          show_call_on_assistant.value.toString() == "1"
               ? Obx(() {
                   return Padding(
                     padding: const EdgeInsets.all(8.0),
@@ -976,18 +976,33 @@ Future<void> showCallingPopup(response, callLoading, callUser) async {
               ),
             ),
             // Wrap the SingleChildScrollView in a Flexible widget
+            // Flexible(
+            //   child: SizedBox(
+            //     height: 300, // You can also set this dynamically or adjust it
+            //     child: SingleChildScrollView(
+            //       child: Column(
+            //         children: [
+            //           Html(
+            //             shrinkWrap: true,
+            //             data: response.description ?? "",
+            //           ),
+            //         ],
+            //       ),
+            //     ),
+            //   ),
+            // ),
             Flexible(
               child: SizedBox(
                 height: 300, // You can also set this dynamically or adjust it
-                child: SingleChildScrollView(
-                  child: Column(
-                    children: [
-                      Html(
+                child: Column(
+                  children: [
+                    Expanded(
+                      child: Html(
                         shrinkWrap: true,
                         data: response.description ?? "",
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
             ),
